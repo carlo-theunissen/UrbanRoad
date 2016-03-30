ca80832b45e600e1b0c77202d90f9c08f78fa078
carlo theunissen <carlotheunissen96@gmail.com>
2016-03-30 11:07:44 +0200

    trying

--- Assets/C# Scripts/Game/Hub/GUI.cs
+++ Assets/C# Scripts/Game/Hub/GUI.cs
@@ -61,7 +61,7 @@ namespace Game
 		private Vector3 getPosition(int index){
 			Vector2 dimensions = getDimensions ();
 			if (index > 4) {
-				return new Vector2 (dimensions.x, dimensions.y * (index - 5));
+				return new Vector2 (dimensions.x + 10, (dimensions.y + 7) * (index - 6) - 20);
 			}
 			return new Vector2 (0, -(dimensions.y + 7)* index - 20);
 		}
