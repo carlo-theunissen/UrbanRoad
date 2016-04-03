0a760ec53a33e591e97dbcc3cbcb5c5d0cdd9049
Dylano Hartman <dylano.hartman@gmail.com>
2016-04-04 01:39:05 +0200

    Made some progress with the credits screen.

---
+++ Assets/C# Scripts/Menu/Credits.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Credits : MonoBehaviour {
+
+    float speed = 2;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.up * speed, Space.World);
+
+        if (transform.position.y > 900)
+        {
+            SceneManager.LoadScene("Menu_start");
+
+        }
+    }
+}
