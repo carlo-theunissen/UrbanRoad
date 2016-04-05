77f411d6e76c796a5f03e536e031c577e3bcd7e2
carlo theunissen <carlotheunissen96@gmail.com>
2016-04-05 11:47:46 +0200

    info popup

---
+++ Assets/C# Scripts/Game/Hub/InfoButton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+
+namespace Game
+{
+	public class InfoButton : MonoBehaviour,IPointerClickHandler {
+		public Popup infoButton;
+		public void OnPointerClick(PointerEventData eventData){
+			AudioProvider.getInstance().playAudio("Standard Button");
+			if (infoButton.isShown ()) {
+				infoButton.OutAnimation ();
+			} else {
+				infoButton.Display ();
+			}
+		}
+	}
+}
+
