0c8fc95ab19a7af4687d02a9d5f39d09f624d98c
carlo theunissen <carlotheunissen96@gmail.com>
2016-04-06 10:22:44 +0200

    Delete unused files and let help popup show on first level

---
+++ Assets/C# Scripts/Game/Hub/InfoPopup.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+namespace Game 
+{
+	public class InfoPopup : Popup, IPointerClickHandler
+	{
+		public void OnPointerClick(PointerEventData eventData){
+			OutAnimation ();
+		}
+	}
+}
+
